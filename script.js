67a354d810c1e0a862a28f3cb1283cc51b53c7d1
paclem <pascal.moeller@posteo.de>
2023-06-18 22:43:21 +0200

    started project

---
+++ script.js
@@ -0,0 +1,19 @@
+let currentPokemon;
+
+
+async function loadPokemon(){
+    let url = `https://pokeapi.co/api/v2/pokemon/charmander`
+    let response = await fetch(url);
+    currentPokemon = await response.json();
+    console.log(currentPokemon);
+    console.log(currentPokemon['sprites']['other']['official-artwork']['front_default']);
+
+
+    renderPokemonInfo();
+}
+
+
+function renderPokemonInfo(){
+    document.getElementById('pokemonName').innerHTML = currentPokemon['name'];
+    document.getElementById('pokemonImage').src = currentPokemon['sprites']['other']['official-artwork']['front_default'];
+}
